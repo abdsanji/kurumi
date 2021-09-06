5ccaea2381720af05d352d37b3a4e8037360a9ec
abdsanji <89727732+abdsanji@users.noreply.github.com>
2021-09-06 11:49:57 +0800

    Update __main__.py

--- KURUMIBOT/__main__.py
+++ KURUMIBOT/__main__.py
@@ -69,8 +69,8 @@ For all command use / [or](https://telegra.ph/file/b1b545eb4877b012eac82.png) !
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\nKindly use ! for commands if / is not working\n")
 
-TOKISAKI_IMG = "https://telegra.ph/file/19896d4051fd8b2e5cc0e.jpg"
-KURUMI_IMG = "https://telegra.ph/file/17dd887f982b72947089e.jpg"
+SANJI_IMG = "https://telegra.ph/file/19896d4051fd8b2e5cc0e.jpg"
+SORA_IMG = "https://telegra.ph/file/17dd887f982b72947089e.jpg"
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 Click here to donate in [Paypal](https://www.paypal.me/zameeljaz)"""
